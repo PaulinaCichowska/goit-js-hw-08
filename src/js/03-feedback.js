178ac4497466ac25b9469acb1ea59cc0ca998248
PaulinaCichowska <Paulina_Cichowska@hotmail.com>
2023-07-12 12:28:30 +0200

    xd

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,26 @@
+const throttle = require('lodash.throttle');
+const email = document.querySelector('input');
+const message = document.querySelector('textarea');
+const feedbackForm = document.querySelector('form');
 
+document.addEventListener('DOMContentLoaded', e => {
+  let formState = JSON.parse(localStorage.getItem('feedback-form-state'));
+  email.value = formState.email;
+  message.value = formState.message;
+});
+
+feedbackForm.addEventListener('input', throttle(storageSave, 500));
+
+function storageSave(e) {
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify({ email: email.value, message: message.value })
+  );
+}
+
+feedbackForm.addEventListener('submit', e => {
+  e.preventDefault();
+  console.log({ email: email.value, message: message.value });
+  e.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+});
