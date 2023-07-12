f55a916593ccc49db888567f2a2f1941048fab56
PaulinaCichowska <Paulina_Cichowska@hotmail.com>
2023-07-12 11:06:52 +0200

    y

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,33 +1,38 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+
 import SimpleLightbox from 'simplelightbox';
-// Dodatkowy import stylów
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
 const gallery = document.querySelector('.gallery');
+// let lightbox = new SimpleLightbox('.gallery__link');
 
 galleryItems.forEach(e => {
   let galleryItem = `<div class="gallery__item">
-    <a class="gallery__link" href="large-image.jpg">
+    <a class="gallery__link" href="${e.original}">
       <img
         class="gallery__image"
         src="${e.preview}"
         data-source="${e.original}"
         alt="${e.description}"
       />
-    </a>
+    </a>+
   </div>`;
   gallery.insertAdjacentHTML('beforeend', galleryItem);
 });
 
-gallery.addEventListener('click', e => {
-  e.preventDefault();
-  let target = e.target;
-  let bigImage = target.getAttribute('data-source');
+// gallery.addEventListener('click', e => {
+//   e.preventDefault();
+//   let target = e.target;
+//   let bigImage = target.getAttribute('data-source');
 
-  if (bigImage != null) {
-    basicLightbox.create(`<img src="${bigImage}">`).show();
-  }
+//   if (bigImage != null) {
+
+//   }
+// });
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionType: 'alt',
 });
+// console.log(lightbox);
